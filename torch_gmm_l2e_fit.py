f034e12d9d55f9634ab8bf23ec3ea3a5c7ddc589
Jamie McCann <jamie.mccann@radarservices.com>
2019-08-27 16:49:11 +0200

    Working L2E module ported to torch

--- torch_gmm_l2e_fit.py
+++ torch_gmm_l2e_fit.py
@@ -5,70 +5,11 @@ from torch.nn import Linear, Module
 from torch.distributions import MultivariateNormal as MVN
 from torch.utils.tensorboard import SummaryWriter
 
-torch.manual_seed(0)
-
 import numpy as np
-
-def np_softplus(x):
-    return np.log(1 + np.exp(-np.abs(x))) + np.maximum(x, 0)
-
-
-def np_softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
-
-
-def np_sigmoid(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def build_toy_dataset(N, K=None, D=None, pi=None):
-    pi = np.array([0.4, 0.6])
-    mus = [[2, 2], [-2, -2]]
-    stds = [[[1, 0.5], [0.5, 1]], [[1, -0.5], [-0.5, 1]]]
-    x = np.zeros((N, 3), dtype=np.float32)
-    for n in range(N):
-        k = np.argmax(np.random.multinomial(1, pi))
-        x[n, 0:2] = np.random.multivariate_normal(mus[k], stds[k])
-        x[n, 2] = k
-    return x
 
 sigmoid = torch.nn.Sigmoid()
 softmax = torch.nn.Softmax(dim=0)
 softplus = torch.nn.Softplus(beta=1)
-
-def make_scale_tril(gammas, dim, full_cov=True):
-    if not(full_cov):
-        return torch.diag_embed(gammas)
-    d_vals = gammas[:, 0:dim]
-    od_vals = gammas[:, dim:]
-    d = d_vals.shape[-1]
-    diags = torch.arange(d) + torch.cumsum(torch.arange(d), dim=0)
-    tester = torch.zeros(d_vals.shape[0], d*(d+1)//2)
-    tester[:, diags] = d_vals
-    od0, od1 = (tester == 0).nonzero().transpose(0, 1)
-    tester[od0, od1] = od_vals.flatten()
-    i,j = torch.tril_indices(d,d)
-    tril = torch.zeros(*d_vals.shape, d)
-    tril[:, i, j] = tester
-    return tril
-
-def make_scale_tril_not_convoluted(diags, off_diags=None):
-    if off_diags is None:
-        return torch.diag_embed(diags)
-    k = diags.shape[0]
-    d = diags.shape[-1]
-    d_idx0, d_idx1 = torch.cat(
-        (torch.arange(d).unsqueeze(0),
-         torch.arange(d).unsqueeze(0))
-    )
-    o_idx = torch.tril_indices(d, d)[0] != torch.tril_indices(d, d)[1]
-    o_idx0, o_idx1 = torch.tril_indices(d, d)[:, o_idx]
-    tril = torch.zeros(k, d, d)
-    tril[:, range(d), range(d)] = diags
-    tril[:, o_idx0, o_idx1] = off_diags
-    return tril
-
 
 def update_tril(entries, D):
     tril = torch.zeros(D, D)
@@ -81,13 +22,15 @@ def update_tril(entries, D):
 def update_mvns(pi, mu_opt, tril_entries):
     mvns = [
         MVN(
-            loc=mu_opt[i], scale_tril=update_tril(tril_entries[i], D), validate_args=True
-        ) for i in range(K)
+            loc=mu_opt[i], scale_tril=update_tril(
+                tril_entries[i], mu_opt.shape[-1]
+            ), validate_args=True
+        ) for i in range(pi.shape[0])
     ]
 
     diffs = []
-    for i in range(K):
-        for j in range(K):
+    for i in range(pi.shape[0]):
+        for j in range(pi.shape[0]):
             m = MVN(
                 mu_opt[i] - mu_opt[j],
                 covariance_matrix=mvns[i].covariance_matrix + mvns[j].covariance_matrix,
@@ -102,6 +45,7 @@ class GaussianMixtureModel(Module):
         super().__init__()
 
         # model structure
+        init = torch.nn.init.normal_
         self.k = num_mixtures
         self.d = num_dim
         self.full_cov = full_cov
@@ -113,22 +57,27 @@ class GaussianMixtureModel(Module):
             self.weights_bijector = sigmoid
 
         # initialize parameters
-        self.weights = torch.nn.Parameter(
-            torch.randn(self.k), requires_grad=True
-        )
-        self.mus = torch.nn.Parameter(
-            torch.randn(self.k, self.d), requires_grad=True
-        )
-        self.diags = torch.nn.Parameter(
-            torch.randn(self.k, self.d), requires_grad=True
+        self.pi = init(torch.empty(self.k, requires_grad=True))
+
+        self.mu = init(
+            torch.empty(self.k, self.d, requires_grad=True)
         )
 
         if full_cov:
-            self.off_diags = torch.nn.Parameter(
-                torch.randn(
-                    self.k, self.d * (self.d - 1) // 2
-                ), requires_grad=True
+            self.tril_entries = init(
+                torch.empty(
+                    self.k, self.d * (self.d + 1) // 2, requires_grad=True
+                )
             )
+
+        else:
+            self.tril_entries = init(
+                torch.empty(self.k, self.d, requires_grad=True)
+            )
+
+        self.parameters_list = [self.pi, self.mu, self.tril_entries]
+        self.mvns, self.diffs = update_mvns(*self.parameters_list)
+
 
         use_cuda = torch.cuda.is_available()
         self.device = torch.device(cuda_dev if use_cuda else "cpu")
@@ -137,55 +86,66 @@ class GaussianMixtureModel(Module):
             self.cuda()
 
     def forward(self, inputs):
-        diags = softplus(self.diags)
-        weights = self.weights_bijector(self.weights)
-        chols = make_scale_tril_not_convoluted(diags, self.off_diags)
-        probs = []
-        for i in range(self.k):
-            mvn = MVN(self.mus[i], scale_tril=chols[i])
-            probs.append(weights[i] * mvn.log_prob(inputs).exp().unsqueeze(1))
+        self.pi = self.weights_bijector(self.pi)
+        self.mvns, self.diffs = update_mvns(*self.parameters_list)
 
-        # mvns = MVN(self.mus, scale_tril=chols)
-        # probs = mvns.log_prob(inputs).exp()
-        # print(probs.shape)
+    def compute_probs_loss(self, inputs):
+        loss = 0
+        for x in range(self.k):
+            loss += (self.pi[x] * self.mvns[x].log_prob(inputs).exp()).sum()
+        return (-2/inputs.shape[0]) * loss
 
-        l2 = []
-        covs = torch.bmm(chols, torch.transpose(chols, dim0=1, dim1=2))
-        for i in range(self.k):
-            for j in range(self.k):
-                diff = MVN(
-                    self.mus[i] - self.mus[j],
-                    covariance_matrix=covs[i]+covs[j],
-                    validate_args=True
-                )
-                p = diff.log_prob(torch.zeros(self.d)).exp() * weights[i] * weights[j]
-                l2.append(p)
-
-        return torch.Tensor(l2), torch.cat(probs, dim=1).sum(dim=1)
+    def compute_l2_loss(self):
+        l2_loss = 0
+        for d, w1, w2 in self.diffs:
+            l2_loss += d.log_prob(
+                torch.zeros(self.d)
+            ).exp() * w1 * w2
+        return l2_loss
 
 
-    def fit(self, inputs, nepochs=500):
+    def fit(self, inputs, nepochs=1000, starts=1, log_interval=10):
 
         optimizer = torch.optim.Adam
-        opt = optimizer(self.parameters(), lr=0.1)
+        opt = optimizer(self.parameters_list, lr=0.01)
+        for x in range(starts):
+            for i in range(nepochs):
+                opt.zero_grad()
+                loss = self.compute_probs_loss(inputs) + self.compute_l2_loss()
+                loss.backward(retain_graph=True)
+                opt.step()
+                self.mvns, self.diffs = update_mvns(
+                    *self.parameters_list
+                )
+                if i % log_interval == 0:
+                    print(f"{x} {i}: {loss.item()}")
 
-        for i in range(nepochs):
-            opt.zero_grad()
-            outputs = self.forward(inputs)
-            loss = outputs[0].sum() - 2 * outputs[1].mean()
-            loss.backward()
-            # if i % 10 == 0:
-                # print(outputs[0].shape)
-                # print(i, loss.item(), outputs[0].sum(), -2*outputs[1].mean(), outputs[1].shape)
-            opt.step()
-        print("fit finished")
+import sys
+
+np.random.seed(int(sys.argv[1]))
+torch.manual_seed(int(sys.argv[2]))
 
 
-np.random.seed(4)
-data = build_toy_dataset(500)
+# simulate some data
+N = 500
+D = 2
+K = 2
+
+# true parameters of the model
+weights = np.array([0.65, 0.35])
+mus = np.array([[-1.0, -1.0], [1.0, 1.0]])
+covs = np.array([
+    [[0.11, -0.05], [-0.05, 0.11]],
+    [[0.33, 0.100], [0.100, 0.33]]
+])
+
+X = np.zeros((N,D+1)) # accomodate labels in last col
+for i in range(N):
+    k = np.argmax(np.random.multinomial(1, weights))
+    X[i, 0:2] = np.random.multivariate_normal(mus[k], covs[k])
+    X[i, 2] = k
+
+inputs = torch.Tensor(X[:, 0:2])
+
 GMM = GaussianMixtureModel()
-print(np_softmax(GMM.weights.detach().numpy()))
-c = torch.Tensor(data[:, 0:2])
-GMM.fit(c)
-print(np_softmax(GMM.weights.detach().numpy()))
-
+GMM.fit(inputs, nepochs=1000, log_interval=100)
